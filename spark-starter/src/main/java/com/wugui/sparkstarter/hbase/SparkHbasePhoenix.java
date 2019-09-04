b0f4231ea669c093635a8881258f283bc7676069
huzekang <1040080742@qq.com>
2019-09-04 09:50:44 +0800

    change:1.更新hbase相关

--- spark-starter/src/main/java/com/wugui/sparkstarter/hbase/SparkHbasePhoenix.java
+++ spark-starter/src/main/java/com/wugui/sparkstarter/hbase/SparkHbasePhoenix.java
@@ -1,8 +1,6 @@
 package com.wugui.sparkstarter.hbase;
 
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.*;
 
 /**
  * @program: bigdata-starter
@@ -10,7 +8,7 @@ import org.apache.spark.sql.SparkSession;
  * @create: 2019-08-30 18:07
  **/
 public class SparkHbasePhoenix {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws AnalysisException {
         SparkSession sparkSession = SparkSession.builder().appName("SparkHBaseDataFrame").master("local").getOrCreate();
 
         Dataset<Row> dataset = sparkSession.read()
@@ -18,8 +16,11 @@ public class SparkHbasePhoenix {
                 .option("driver", "org.apache.phoenix.jdbc.PhoenixDriver")
                 .option("phoenix.schema.isNamespaceMappingEnabled", "true")
                 .option("url", "jdbc:phoenix:cdh01:2181")
-                .option("dbtable", "userInfo")
+                .option("dbtable", "patient_test")
                 .load();
         dataset.printSchema();
+        dataset.select("ID").show();
+        dataset.createTempView("abc");
+        sparkSession.sql("select empi,id from abc  where id> 1005  ").show();
     }
 }
