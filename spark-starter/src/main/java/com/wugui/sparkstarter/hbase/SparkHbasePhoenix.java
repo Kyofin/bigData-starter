f049cddb3c248155380a791c404ce6cdce75fc4d
huzekang <1040080742@qq.com>
2019-09-05 18:17:59 +0800

    add: 1.phoniex多种接入方式

--- spark-starter/src/main/java/com/wugui/sparkstarter/hbase/SparkHbasePhoenix.java
+++ spark-starter/src/main/java/com/wugui/sparkstarter/hbase/SparkHbasePhoenix.java
@@ -1,6 +1,10 @@
 package com.wugui.sparkstarter.hbase;
 
+import org.apache.spark.api.java.function.MapFunction;
 import org.apache.spark.sql.*;
+import static org.apache.spark.sql.functions.*;
+import org.apache.phoenix.spark.*;
+
 
 /**
  * @program: bigdata-starter
@@ -11,6 +15,7 @@ public class SparkHbasePhoenix {
     public static void main(String[] args) throws AnalysisException {
         SparkSession sparkSession = SparkSession.builder().appName("SparkHBaseDataFrame").master("local").getOrCreate();
 
+        // 使用phoenix jdbc连接驱动读取数据
         Dataset<Row> dataset = sparkSession.read()
                 .format("jdbc")
                 .option("driver", "org.apache.phoenix.jdbc.PhoenixDriver")
@@ -18,9 +23,20 @@ public class SparkHbasePhoenix {
                 .option("url", "jdbc:phoenix:cdh01:2181")
                 .option("dbtable", "patient_test")
                 .load();
-        dataset.printSchema();
-        dataset.select("ID").show();
-        dataset.createTempView("abc");
-        sparkSession.sql("select empi,id from abc  where id> 1005  ").show();
+        dataset.show();
+
+        // 使用spark sql 的函数替换phone列的所有匹配规则的值
+        Dataset<Row> rowDataset = dataset.withColumn("PHONE", regexp_replace(col("PHONE"), "123", "sss"));
+
+        // 使用phoenix spark plugin 写入数据
+        rowDataset
+                .write()
+                .format("org.apache.phoenix.spark")
+                .mode(SaveMode.Overwrite)
+                .option("table", "patient_test")
+                .option("zkUrl", "cdh01:2181")
+                .save();
+
+
     }
 }
